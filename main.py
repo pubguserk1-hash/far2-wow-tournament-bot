c47ed649855dbb92227f24b8e966fa29c2f8ed03
pubguserk1-hash <pubguserk1@gmail.com>
2026-06-26 13:01:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+
+TOKEN = os.getenv("8734240091:AAFblLG38MqeurDkHEB41KbV9uYsvHNd-sQ")
+ADMIN_ID = int(os.getenv("8079009420"))
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(F.text == "/start")
+async def start(message: Message):
+    await message.answer(
+        "Отправьте пожалуйста:\n"
+        "Название команды | никнеймы | юзернейм кто регистрирует"
+    )
+
+@dp.message()
+async def get_data(message: Message):
+    text = message.text
+
+    await bot.send_message(
+        ADMIN_ID,
+        f"📥 Новая регистрация:\n\n{text}\n\nОт: @{message.from_user.username}"
+    )
+
+    await message.answer("✅ Ваша команда успешно зарегистрирована")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
