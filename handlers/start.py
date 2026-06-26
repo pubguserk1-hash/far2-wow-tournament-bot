05eb9b160143ab4b05ce8c1fa7b6372f6bd59493
pubguserk1-hash <pubguserk1@gmail.com>
2026-06-26 11:38:48 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,41 @@
-о
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from keyboards import main_menu
+
+from database import teams_count
+
+from config import (
+    ADMIN_IDS,
+    TOURNAMENT_NAME,
+    PRIZE
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    user = update.effective_user
+
+    text = f"""
+🏆 <b>{TOURNAMENT_NAME}</b>
+
+🎁 <b>Призовой фонд:</b>
+
+{PRIZE}
+
+👥 Зарегистрировано команд:
+<b>{teams_count()}</b>
+
+━━━━━━━━━━━━━━
+
+Для регистрации нажмите
+
+📝 <b>Регистрация</b>
+
+⚠️ Можно участвовать только один раз под одним названием команды.
+"""
+
+    await update.message.reply_html(
+        text,
+        reply_markup=main_menu(user.id in ADMIN_IDS)
+    )
